c77dacab81bba504150d0c6b2d88c6ba65a86add
xanish <danishali7866@gmail.com>
2024-07-21 15:04:39 +0530

    refactor: made all game properties private

--- blackjack/game.go
+++ blackjack/game.go
@@ -14,17 +14,17 @@ const (
 )
 
 type Game struct {
-	Deck   deck_of_cards.Deck
+	deck   deck_of_cards.Deck
 	state  state
-	Player Hand
-	Dealer Hand
+	player Hand
+	dealer Hand
 }
 
 func New() Game {
 	return Game{
-		Deck:   deck_of_cards.NewDeck(deck_of_cards.Packs(3), deck_of_cards.Shuffle),
-		Player: Hand{cards: make([]deck_of_cards.Card, 0)},
-		Dealer: Hand{cards: make([]deck_of_cards.Card, 0)},
+		deck:   deck_of_cards.NewDeck(deck_of_cards.Packs(3), deck_of_cards.Shuffle),
+		player: Hand{cards: make([]deck_of_cards.Card, 0)},
+		dealer: Hand{cards: make([]deck_of_cards.Card, 0)},
 		state:  statePlayerTurn,
 	}
 }
@@ -35,8 +35,8 @@ func (g *Game) Play() {
 	var input string
 	for g.state == statePlayerTurn {
 		fmt.Println()
-		fmt.Printf("Player hand: %s\n", g.Player)
-		fmt.Printf("Dealer hand: %s\n", g.Dealer.DealerString())
+		fmt.Printf("player hand: %s\n", g.player)
+		fmt.Printf("dealer hand: %s\n", g.dealer.DealerString())
 		fmt.Print("\nWhat would you like to do? (h)it or (s)tand... ")
 		_, _ = fmt.Scanln(&input)
 
@@ -53,7 +53,7 @@ func (g *Game) Play() {
 	for g.state == statePlayerTurn {
 		// If dealer score <= 16, we hit
 		// If dealer has a soft 17, then we hit.
-		if g.Dealer.Score() <= 16 || (g.Dealer.Score() == 17 && g.Dealer.MinScore() != 17) {
+		if g.dealer.Score() <= 16 || (g.dealer.Score() == 17 && g.dealer.MinScore() != 17) {
 			Hit(g)
 		} else {
 			Stand(g)
@@ -64,22 +64,22 @@ func (g *Game) Play() {
 }
 
 func (g *Game) String() string {
-	return fmt.Sprintf("current deck size: %d\n state: %v\n player hand: %s\n dealer hand: %s\n", g.Deck.RemainingCards(), g.state, g.Player, g.Dealer)
+	return fmt.Sprintf("current deck size: %d\n state: %v\n player hand: %s\n dealer hand: %s\n", g.deck.RemainingCards(), g.state, g.player, g.dealer)
 }
 
 func (g *Game) currentPlayer() *Hand {
 	switch g.state {
 	case statePlayerTurn:
-		return &g.Player
+		return &g.player
 	case stateDealerTurn:
-		return &g.Dealer
+		return &g.dealer
 	default:
 		panic("it isn't currently any player's turn")
 	}
 }
 
 func Hit(g *Game) {
-	pick, err := g.Deck.Draw()
+	pick, err := g.deck.Draw()
 	if err != nil {
 		panic(err)
 	}
@@ -97,27 +97,27 @@ func Stand(g *Game) {
 }
 
 func deal(g *Game) {
-	g.Player.cards = make([]deck_of_cards.Card, 0, 2)
-	g.Dealer.cards = make([]deck_of_cards.Card, 0, 2)
+	g.player.cards = make([]deck_of_cards.Card, 0, 2)
+	g.dealer.cards = make([]deck_of_cards.Card, 0, 2)
 
 	for i := 0; i < 2; i++ {
-		draw(&g.Player, &g.Deck)
-		draw(&g.Dealer, &g.Deck)
+		draw(&g.player, &g.deck)
+		draw(&g.dealer, &g.deck)
 	}
 
 	g.state = statePlayerTurn
 }
 
 func end(g *Game) {
-	pScore, dScore := g.Player.Score(), g.Dealer.Score()
+	pScore, dScore := g.player.Score(), g.dealer.Score()
 	fmt.Println("\nFinal hands:")
-	fmt.Printf("Player hand: %s (Score: %d)\n", g.Player, pScore)
-	fmt.Printf("Dealer hand: %s (Score: %d)\n\n", g.Dealer, dScore)
+	fmt.Printf("player hand: %s (Score: %d)\n", g.player, pScore)
+	fmt.Printf("dealer hand: %s (Score: %d)\n\n", g.dealer, dScore)
 	switch {
 	case pScore > 21:
 		fmt.Println("Result: You busted")
 	case dScore > 21:
-		fmt.Println("Result: Dealer busted")
+		fmt.Println("Result: dealer busted")
 	case pScore > dScore:
 		fmt.Println("Result: You win!")
 	case dScore > pScore:
