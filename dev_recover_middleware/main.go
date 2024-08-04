8596c1867371cc168923ff8bbdafea3e66acc6fa
xanish <danishali7866@gmail.com>
2024-08-04 19:29:41 +0530

    add line highlighting

--- dev_recover_middleware/main.go
+++ dev_recover_middleware/main.go
@@ -9,9 +9,12 @@ import (
 	"net/url"
 	"os"
 	"runtime/debug"
+	"strconv"
 	"strings"
 
-	"github.com/alecthomas/chroma/quick"
+	"github.com/alecthomas/chroma/formatters/html"
+	"github.com/alecthomas/chroma/lexers"
+	"github.com/alecthomas/chroma/styles"
 )
 
 func main() {
@@ -25,6 +28,12 @@ func main() {
 
 func sourceCodeHandler(w http.ResponseWriter, r *http.Request) {
 	path := r.FormValue("path")
+	lineStr := r.FormValue("line")
+	line, err := strconv.Atoi(lineStr)
+	if err != nil {
+		line = -1
+	}
+
 	file, err := os.Open(path)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -38,7 +47,24 @@ func sourceCodeHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	_ = quick.Highlight(w, b.String(), "go", "html", "github")
+	var lines [][2]int
+	if line > 0 {
+		lines = append(lines, [2]int{line, line})
+	}
+
+	lexer := lexers.Get("go")
+	iterator, err := lexer.Tokenise(nil, b.String())
+
+	style := styles.Get("github")
+	if style == nil {
+		style = styles.Fallback
+	}
+
+	formatter := html.New(html.TabWidth(2), html.WithLineNumbers(true), html.LineNumbersInTable(true), html.HighlightLines(lines))
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<style>pre { font-size: 1.2em; }</style>")
+	formatter.Format(w, style, iterator)
+	// _ = quick.Highlight(w, b.String(), "go", "html", "github")
 }
 
 func devMw(app http.Handler) http.HandlerFunc {
@@ -88,9 +114,18 @@ func makeLinks(stack string) string {
 			}
 		}
 
+		var lineStr strings.Builder
+		for i := len(file) + 2; i < len(line); i++ {
+			if line[i] < '0' || line[i] > '9' {
+				break
+			}
+			lineStr.WriteByte(line[i])
+		}
+
 		v := url.Values{}
 		v.Set("path", file)
-		lines[li] = "\t<a href=\"/debug/?" + v.Encode() + "\">" + file + "</a>" + line[len(file)+1:]
+		v.Set("line", lineStr.String())
+		lines[li] = "\t<a href=\"/debug/?" + v.Encode() + "\">" + file + ":" + lineStr.String() + "</a>" + line[len(file)+2+len(lineStr.String()):]
 	}
 
 	return strings.Join(lines, "\n")
